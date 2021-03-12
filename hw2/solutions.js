ad88b798d08205b723e3130d968f0a09abb8736d
hayleytaylor29 <hft929@gmail.com>
2021-03-11 20:32:20 -0500

    verbal questions completed

---
+++ hw2/solutions.js
@@ -0,0 +1,3 @@
+//1. A parameter is the name that is passed into the function, and an argument is the actual value
+// that you are passing through the function
+//2. Return will show the value on the page, console.log will only show you in the console
