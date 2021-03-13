fe8a1a696669f32b7bb37fea66618dfa378f033f
hayleytaylor29 <hft929@gmail.com>
2021-03-12 21:08:38 -0500

    sum array completed

--- hw2/solutions.js
+++ hw2/solutions.js
@@ -1,3 +1,25 @@
 //1. A parameter is the name that is passed into the function, and an argument is the actual value
 // that you are passing through the function
 //2. Return will show the value on the page, console.log will only show you in the console
+
+//Write a function called checkPalindrome that returns a boolean
+// function checkPalindrome(str){
+// //change the string to lowercase
+// const lower = str.toLowerCase();
+// }
+// for (let i = 0; i < str.lower; i++)
+//     if (str[i] !== ) {
+//         return false;
+//     }
+//         return true;
+// }
+
+
+function sumArray(arr){
+    let sum=0;
+    for (let val of arr) {
+        sum += val;
+    }
+     console.log(sum);
+}
+sumArray ([1, 2, 3, 4, 27]);
